d769a929e2981910730e44b11c0f158fef64a1a9
Ryan Davis <94387716+sdl1wrkhremb@users.noreply.github.com>
2023-08-21 11:40:40 -0400

    test comment

--- chapter-04/4-8_cubes.py
+++ chapter-04/4-8_cubes.py
@@ -1,5 +1,7 @@
-#make a list of the first 10 cubes and print using for loop
-cubes = [value**3 for value in range(1,11)]
-               
+# make a list of the first 10 cubes and print using for loop
+cubes = [value**3 for value in range(1, 11)]
+
+# test comment
+
 for cube in cubes:
     print(cube)
