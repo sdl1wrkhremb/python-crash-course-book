5873ba1699f64edfed2a251358661cd271e01b6a
Ryan Davis <94387716+sdl1wrkhremb@users.noreply.github.com>
2023-08-23 17:45:15 -0400

    added exercise

---
+++ chapter_09/9.1_restaurant.py
@@ -0,0 +1,30 @@
+class Restaurant:
+    """a simple attempt to represent a resturant"""
+
+    def __init__(self, resturant_name, cuisine_type):
+        """initialize resturant name and cuisine"""
+        self.name = resturant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        """provide the resturant description"""
+        print(f"{self.name.title()} serves wonderful {self.cuisine_type}!")
+
+    def open_restaurant(self):
+        """simulate opening the resturant"""
+        print(f"{self.name.title()} is now open for business!")
+
+
+red_lobster = Restaurant('red lobster', 'seafood')
+
+print(red_lobster.name.title())
+print(red_lobster.cuisine_type.title())
+
+red_lobster.describe_restaurant()
+red_lobster.open_restaurant()
+
+ludvigs = Restaurant("ludvig's bistro", 'seafood')
+ludvigs.describe_restaurant()
+
+mango_thai = Restaurant('mango thai', 'thai food')
+mango_thai.describe_restaurant()
