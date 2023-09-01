f38d207b8ef34af811d3573b37ef703113e0fb6e
Ryan Davis <94387716+sdl1wrkhremb@users.noreply.github.com>
2023-09-01 12:06:54 -0400

    improved code

--- chapter_09/lottery.py
+++ chapter_09/lottery.py
@@ -1,32 +1,42 @@
-from random import randint
-from random import random
-
-my_ticket = ['A', 'E', '8', 'X']
-lottery_pool = ['A', 'J', 'E', 'X', 'S', '1',
-                '2', '3', '4', '5', '6', '7', '8', '9', '10']
-winning_ticket = []
-# add 10 numbers and five letters to the pool
-# for x in range(1, 10):
-#     number = randint(1, 100)
-#     lottery_pool.append(number)
+from random import choice
 
 
-def draw_winning_ticket():
+def draw_winning_ticket(possibilities):
+    winning_ticket = []
+    for x in range(1, 5):
+        # winning_ticket.append(lottery_pool[randint(0, len(lottery_pool)-1)])
+        winning_ticket.append(choice(possibilities))
+    return winning_ticket
+
+
+def geneate_random_ticket(possibilities):
     ticket = []
     for x in range(1, 5):
-        ticket.append(lottery_pool[randint(0, len(lottery_pool)-1)])
+        # winning_ticket.append(lottery_pool[randint(0, len(lottery_pool)-1)])
+        ticket.append(choice(possibilities))
     return ticket
 
 
-# print(len(lottery_pool))
-# winning_ticket = draw_winning_ticket()
-# print(winning_ticket)
+def check_ticket(my_ticket, winning_ticket):
+    if my_ticket == winning_ticket:
+        return True
+    else:
+        return False
+
+
+lottery_pool = ['A', 'J', 'E', 'X', 'S', '1',
+                '2', '3', '4', '5', '6', '7',
+                '8', '9', '10']
+my_ticket = geneate_random_ticket(lottery_pool)
 
 # loop until my ticket wins
 drawings = 0
 while True:
-    winning_ticket = draw_winning_ticket()
+    winning_ticket = draw_winning_ticket(lottery_pool)
+    won = check_ticket(my_ticket, winning_ticket)
     drawings += 1
-    if my_ticket == winning_ticket:
+    if won:
         print(f"you finally won after {drawings} drawings!")
+        print(f"your ticket: {my_ticket}")
+        print(f"winning ticket: {winning_ticket}")
         break
